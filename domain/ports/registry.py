36a4820af916b16883f368a28124dbe4cc15ed24
HaoNgo232 <ngogiahao2018@gmail.com>
2026-06-15 16:52:47 +0700

    feat: extend DomainRegistry with Clean Architecture ports

--- domain/ports/registry.py
+++ domain/ports/registry.py
@@ -1,10 +1,23 @@
-from typing import Callable, Optional
+from typing import Callable, Optional, Any
 from domain.ports.tokenization_port import ITokenizationService
 from domain.ports.workspace_scanner import IWorkspaceScanner
 from domain.ports.directory_scanner import IDirectoryScanner
 from domain.workflow.interfaces.git_port import IGitService
 from domain.workflow.interfaces.ast_parser_port import IAstParser
 from domain.config.app_settings import AppSettings
+
+from domain.ports.action_result import ActionResult
+from domain.ports.ai_port import IAIProvider
+from domain.ports.repo_manager_port import IRepoManager
+from domain.ports.settings_service_port import ISettingsService
+from domain.ports.ignore_engine_port import IIgnoreEngine
+from domain.ports.cache_registry_port import ICacheRegistry
+from domain.ports.file_actions_port import IFileActionsService
+from domain.ports.recent_folders_port import IRecentFoldersService
+from domain.ports.session_state_port import ISessionStateService
+from domain.ports.security_scanner_port import ISecurityScanner
+from application.interfaces.file_watcher_port import IFileWatcherService
+from application.services.service_interfaces import IClipboardService
 
 
 class DomainRegistry:
@@ -21,6 +34,17 @@ class DomainRegistry:
     _git_service: Optional[IGitService] = None
     _ast_parser: Optional[IAstParser] = None
     _settings_provider: Optional[Callable[[], AppSettings]] = None
+
+    _security_scanner: Optional[ISecurityScanner] = None
+    _repo_manager: Optional[IRepoManager] = None
+    _settings_service: Optional[ISettingsService] = None
+    _recent_folders: Optional[IRecentFoldersService] = None
+    _session_state: Optional[ISessionStateService] = None
+    _cache_registry: Optional[ICacheRegistry] = None
+    _file_actions_service: Optional[IFileActionsService] = None
+    _file_watcher_service: Optional[IFileWatcherService] = None
+    _clipboard_service: Optional[IClipboardService] = None
+    _ignore_engine: Optional[IIgnoreEngine] = None
 
     @classmethod
     def register_tokenization_service(cls, service: ITokenizationService) -> None:
@@ -83,3 +107,103 @@ class DomainRegistry:
         if cls._settings_provider is None:
             return AppSettings()
         return cls._settings_provider()
+
+    @classmethod
+    def register_security_scanner(cls, scanner: ISecurityScanner) -> None:
+        cls._security_scanner = scanner
+
+    @classmethod
+    def security_scanner(cls) -> ISecurityScanner:
+        if cls._security_scanner is None:
+            raise RuntimeError("ISecurityScanner is not registered")
+        return cls._security_scanner
+
+    @classmethod
+    def register_repo_manager(cls, manager: IRepoManager) -> None:
+        cls._repo_manager = manager
+
+    @classmethod
+    def repo_manager(cls) -> IRepoManager:
+        if cls._repo_manager is None:
+            raise RuntimeError("IRepoManager is not registered")
+        return cls._repo_manager
+
+    @classmethod
+    def register_settings_service(cls, service: ISettingsService) -> None:
+        cls._settings_service = service
+
+    @classmethod
+    def settings_service(cls) -> ISettingsService:
+        if cls._settings_service is None:
+            raise RuntimeError("ISettingsService is not registered")
+        return cls._settings_service
+
+    @classmethod
+    def register_recent_folders(cls, service: IRecentFoldersService) -> None:
+        cls._recent_folders = service
+
+    @classmethod
+    def recent_folders(cls) -> IRecentFoldersService:
+        if cls._recent_folders is None:
+            raise RuntimeError("IRecentFoldersService is not registered")
+        return cls._recent_folders
+
+    @classmethod
+    def register_session_state(cls, service: ISessionStateService) -> None:
+        cls._session_state = service
+
+    @classmethod
+    def session_state(cls) -> ISessionStateService:
+        if cls._session_state is None:
+            raise RuntimeError("ISessionStateService is not registered")
+        return cls._session_state
+
+    @classmethod
+    def register_cache_registry(cls, registry: ICacheRegistry) -> None:
+        cls._cache_registry = registry
+
+    @classmethod
+    def cache_registry(cls) -> ICacheRegistry:
+        if cls._cache_registry is None:
+            raise RuntimeError("ICacheRegistry is not registered")
+        return cls._cache_registry
+
+    @classmethod
+    def register_file_actions_service(cls, service: IFileActionsService) -> None:
+        cls._file_actions_service = service
+
+    @classmethod
+    def file_actions_service(cls) -> IFileActionsService:
+        if cls._file_actions_service is None:
+            raise RuntimeError("IFileActionsService is not registered")
+        return cls._file_actions_service
+
+    @classmethod
+    def register_file_watcher_service(cls, service: IFileWatcherService) -> None:
+        cls._file_watcher_service = service
+
+    @classmethod
+    def file_watcher_service(cls) -> IFileWatcherService:
+        if cls._file_watcher_service is None:
+            raise RuntimeError("IFileWatcherService is not registered")
+        return cls._file_watcher_service
+
+    @classmethod
+    def register_clipboard_service(cls, service: IClipboardService) -> None:
+        cls._clipboard_service = service
+
+    @classmethod
+    def clipboard_service(cls) -> IClipboardService:
+        if cls._clipboard_service is None:
+            raise RuntimeError("IClipboardService is not registered")
+        return cls._clipboard_service
+
+    @classmethod
+    def register_ignore_engine(cls, engine: IIgnoreEngine) -> None:
+        cls._ignore_engine = engine
+
+    @classmethod
+    def ignore_engine(cls) -> IIgnoreEngine:
+        if cls._ignore_engine is None:
+            raise RuntimeError("IIgnoreEngine is not registered")
+        return cls._ignore_engine
